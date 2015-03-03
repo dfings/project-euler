42cf2d099c167fe960d172680078b93874caec96
Dan Fingal-Surma <dfingal@gmail.com>
2015-03-03 15:39:34 -0800

    Pruning impl
    Doesn’t actually speed things up, sadly.

--- cc/problem_493.cc
+++ cc/problem_493.cc
@@ -1,32 +1,40 @@
+#include <array>
+#include <bitset>
 #include <stdio.h>
 #include <stdint.h>
-#include <vector>
 
 constexpr std::int8_t kNumColors = 7;
+using ColorsArray = std::array<std::int8_t, kNumColors>;
+constexpr ColorsArray kEmptyColorsArray = {0, 0, 0, 0, 0, 0, 0};
 constexpr std::int8_t kNumPerColor = 10;
-constexpr std::int16_t kNumToPick = 20;
+constexpr std::int16_t kNumToPick = 11;
 
 struct State {
-  State() : urn(kNumColors, kNumPerColor) {}
+  State() {
+    for (int i = 0; i < kNumColors; ++i) {
+      urn[i] = kNumPerColor;
+    }
+  }
+  
   std::int64_t total_distinct_colors = 0;
   std::int64_t total_distinct_picks = 0;
-  std::vector<std::int8_t> urn;
+  ColorsArray urn = kEmptyColorsArray;
 };
 
-void CountColors(State* state) {
-  ++state->total_distinct_picks;
+void CountColors(std::int64_t num_equivalent, State* state) {
+  state->total_distinct_picks += num_equivalent;
   for (int i = 0; i < kNumColors; ++i) {
     if (state->urn[i] != kNumPerColor) {
-      ++state->total_distinct_colors;
+      state->total_distinct_colors += num_equivalent;
     }
   }
 }
 
-void PickOneBall(std::int16_t balls_picked, State* state) {
+void PickOneBall_BruteForce(std::int16_t balls_picked, State* state) {
   // Base case: We've picked all the balls, we need to determine how many
   // distinct colors we picked.
   if (balls_picked == kNumToPick) {
-    CountColors(state);
+    CountColors(1, state);
     return;
   }
   
@@ -34,7 +42,40 @@ void PickOneBall(std::int16_t balls_picked, State* state) {
   for (int i = 0; i < kNumColors; ++i) {
     if (state->urn[i] > 0) {
       --state->urn[i];
-      PickOneBall(next_balls_picked, state);
+      PickOneBall_BruteForce(next_balls_picked, state);
+      ++state->urn[i];
+    }
+  }
+}
+
+void PickOneBall_Prune(std::int16_t balls_picked, std::int64_t num_equivalent, State* state) {
+  // Base case: We've picked all the balls, we need to determine how many
+  // distinct colors we picked.
+  if (balls_picked == kNumToPick) {
+    CountColors(num_equivalent, state);
+    return;
+  }
+  
+  ColorsArray next_num_equivalent = kEmptyColorsArray;
+  std::bitset<kNumColors> consumed;
+  for (int i = 0; i < kNumColors; ++i) {
+    std::int8_t count = state->urn[i];
+    if (count != 0 && !consumed[i]) {
+      ++next_num_equivalent[i];
+      for (int j = i + 1; j < kNumColors; ++j) {
+        if (count == state->urn[j]) {
+          consumed.set(j);
+          ++next_num_equivalent[j];
+        }
+      }
+    }
+  }
+  
+  std::int16_t next_balls_picked = balls_picked + 1;
+  for (int i = 0; i < kNumColors; ++i) {
+    if (next_num_equivalent[i] > 0) {
+      --state->urn[i];
+      PickOneBall_Prune(next_balls_picked, num_equivalent * next_num_equivalent[i], state);
       ++state->urn[i];
     }
   }
@@ -42,7 +83,8 @@ void PickOneBall(std::int16_t balls_picked, State* state) {
 
 int main(int argc, char* argv[]) {
   State state;
-  PickOneBall(0, &state);
+  PickOneBall_Prune(0, 1, &state);
+  printf("Total = %lld\n", state.total_distinct_colors);
   printf("Average = %f\n", static_cast<double>(state.total_distinct_colors) / state.total_distinct_picks);
   return 0;
 }
