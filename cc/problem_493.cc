46f5d7074097753b32bb40e2314ff56b17528e3c
Dan Fingal-Surma <dfingal@gmail.com>
2015-03-03 16:15:32 -0800

    Memoized version, but still seems to have bugs...

--- cc/problem_493.cc
+++ cc/problem_493.cc
@@ -1,13 +1,20 @@
+#include <algorithm>
 #include <array>
 #include <bitset>
+#include <map>
 #include <stdio.h>
 #include <stdint.h>
+
+struct Counts {
+  std::int64_t total_distinct_colors = 0;
+  std::int64_t total_distinct_picks = 0;
+};
 
 constexpr std::int8_t kNumColors = 7;
 using ColorsArray = std::array<std::int8_t, kNumColors>;
 constexpr ColorsArray kEmptyColorsArray = {0, 0, 0, 0, 0, 0, 0};
 constexpr std::int8_t kNumPerColor = 10;
-constexpr std::int16_t kNumToPick = 11;
+constexpr std::int16_t kNumToPick = 20;
 
 struct State {
   State() {
@@ -16,75 +23,69 @@ struct State {
     }
   }
   
-  std::int64_t total_distinct_colors = 0;
-  std::int64_t total_distinct_picks = 0;
   ColorsArray urn = kEmptyColorsArray;
+  std::map<int, Counts> memoized;
 };
 
-void CountColors(std::int64_t num_equivalent, State* state) {
-  state->total_distinct_picks += num_equivalent;
+int Encode(const ColorsArray& colors) {
+  ColorsArray sorted = colors;
+  std::sort(sorted.begin(), sorted.end());
+  int encoded = 0;
+  int factor = 1;
   for (int i = 0; i < kNumColors; ++i) {
-    if (state->urn[i] != kNumPerColor) {
-      state->total_distinct_colors += num_equivalent;
-    }
+    encoded += sorted[i] * factor;
+    factor *= kNumPerColor;
   }
+  return encoded;
 }
 
-void PickOneBall_BruteForce(std::int16_t balls_picked, State* state) {
-  // Base case: We've picked all the balls, we need to determine how many
-  // distinct colors we picked.
+int CountColors(const ColorsArray& colors) {
+  int count = 0;
+  for (int i = 0; i < kNumColors; ++i) {
+    if (colors[i] != kNumPerColor) {
+      ++count;
+    }
+  }
+  return count;
+}
+
+Counts PickOneBall(std::int16_t balls_picked, State* state) {
+  int encoded = Encode(state->urn);
+  
   if (balls_picked == kNumToPick) {
-    CountColors(1, state);
-    return;
+    Counts counts;
+    counts.total_distinct_picks = 1;
+    counts.total_distinct_colors = CountColors(state->urn);
+    state->memoized.emplace(encoded, counts);
+    return counts;
   }
   
+  auto it = state->memoized.find(encoded);
+  if (it != state->memoized.end()) {
+    return it->second;
+  } 
+  
+  Counts counts = {};
   std::int16_t next_balls_picked = balls_picked + 1;
   for (int i = 0; i < kNumColors; ++i) {
     if (state->urn[i] > 0) {
       --state->urn[i];
-      PickOneBall_BruteForce(next_balls_picked, state);
+      Counts returned = PickOneBall(next_balls_picked, state);
+      counts.total_distinct_colors += returned.total_distinct_colors;
+      counts.total_distinct_picks += returned.total_distinct_picks;
       ++state->urn[i];
     }
   }
-}
-
-void PickOneBall_Prune(std::int16_t balls_picked, std::int64_t num_equivalent, State* state) {
-  // Base case: We've picked all the balls, we need to determine how many
-  // distinct colors we picked.
-  if (balls_picked == kNumToPick) {
-    CountColors(num_equivalent, state);
-    return;
-  }
   
-  ColorsArray next_num_equivalent = kEmptyColorsArray;
-  std::bitset<kNumColors> consumed;
-  for (int i = 0; i < kNumColors; ++i) {
-    std::int8_t count = state->urn[i];
-    if (count != 0 && !consumed[i]) {
-      ++next_num_equivalent[i];
-      for (int j = i + 1; j < kNumColors; ++j) {
-        if (count == state->urn[j]) {
-          consumed.set(j);
-          ++next_num_equivalent[j];
-        }
-      }
-    }
-  }
-  
-  std::int16_t next_balls_picked = balls_picked + 1;
-  for (int i = 0; i < kNumColors; ++i) {
-    if (next_num_equivalent[i] > 0) {
-      --state->urn[i];
-      PickOneBall_Prune(next_balls_picked, num_equivalent * next_num_equivalent[i], state);
-      ++state->urn[i];
-    }
-  }
+  state->memoized.emplace(encoded, counts);
+  return counts;
 }
 
 int main(int argc, char* argv[]) {
   State state;
-  PickOneBall_Prune(0, 1, &state);
-  printf("Total = %lld\n", state.total_distinct_colors);
-  printf("Average = %f\n", static_cast<double>(state.total_distinct_colors) / state.total_distinct_picks);
+  Counts counts = PickOneBall(0, &state);
+  printf("Total colors = %lld\n", counts.total_distinct_colors);
+  printf("Total picks = %lld\n", counts.total_distinct_picks);
+  printf("Average = %0.9f\n", static_cast<double>(counts.total_distinct_colors) / counts.total_distinct_picks);
   return 0;
 }
