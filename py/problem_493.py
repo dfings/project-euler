9d07f872e8506a8a89d6ed6db76ce6ba17c6d159
Dan Fingal-Surma <dfingal@gmail.com>
2015-04-14 10:09:13 -0700

    There's no need to use floating points.

--- py/problem_493.py
+++ py/problem_493.py
@@ -12,23 +12,22 @@ class Solver(object):
     self.memoized = {}
     
   def CountColors(self):
-    # Must use floating points to get correct precision.
-    count = 0.0  
+    count = 0
     for val in self.urn:
       if val != NUM_PER_COLOR:
-        count += 1.0
+        count += 1
     return count
 
   def PickNextBall(self):
     if self.balls_picked == NUM_TO_PICK:
-      return [1.0, self.CountColors()]
+      return [1, self.CountColors()]
     
     encoded = tuple(sorted(self.urn))
     counts = self.memoized.get(encoded, None)
     if counts:
       return counts
     
-    counts = [0.0, 0.0]
+    counts = [0, 0]
     self.balls_picked += 1
     for i in xrange(NUM_COLORS):
       for j in xrange(NUM_PER_COLOR):
@@ -48,7 +47,7 @@ def main():
   counts = solver.PickNextBall()
   print "Total colors = %d" % counts[1]
   print "Total picks = %d" % counts[0]
-  print "Average = %0.9f" % (counts[1] / counts[0])
+  print "Average = %0.9f" % (float(counts[1]) / counts[0])
   
 
 if __name__ == '__main__':
