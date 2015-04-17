0f1c52b5e1d075564288d60ac23c4bec2edb4cf0
Dan Fingal-Surma <dfingal@gmail.com>
2015-04-17 00:58:19 -0700

    Go impl for problem 3

---
+++ go/problem_3.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	factor, max := 2, 0
+	num := 600851475143
+	for num > 1 {
+		if num % factor == 0 {
+			num = num / factor
+			max = factor
+		} else {
+			factor = factor + 1
+		}
+	}
+	fmt.Printf("%d\n", max)
+}
