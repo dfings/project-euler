a9298e8336b5193f60b1d2ca0a9efcae2459b4fd
Dan Fingal-Surma <dfingal@gmail.com>
2015-04-16 17:43:21 -0700

    Got the caching working.

--- hs/problem_493.hs
+++ hs/problem_493.hs
@@ -3,20 +3,20 @@ import qualified Data.Map.Strict as Map
 
 numColors = 7
 numPerColor = 10
-numToPick = 2
+numToPick = 20
 
 -- Determines if we've picked all the balls we need to from an urn.
 allBallsPicked = (==) (numColors * numPerColor - numToPick) . sum
 
 -- Counts the number of distinct colors that have been picked from the urn.
-countColorsPicked = (length . filter ((/=) numPerColor))
+countColorsPicked = toInteger . length . filter ((/=) numPerColor)
+leaf urn = ((countColorsPicked urn), 1)
 
 getNextStates uhead ballCount utail =
   if ballCount == 0 then [] 
   else if utail == [] then oneChildStatePerBall (uhead ++ [ballCount - 1])
   else oneChildStatePerBall (uhead ++ [ballCount - 1] ++ utail)
   where oneChildStatePerBall = replicate ballCount
-
 
 -- Generates one new urn state for each possible ball that could be picked.
 childGenerator uhead ballCount utail =
@@ -29,8 +29,8 @@ getChildren urn = childGenerator [] (head urn) (tail urn)
 
 -- Tuple support.
 sumTuple (a1, a2) (b1, b2) = (a1 + b1, a2 + b2)
-sumTuples = foldl1 sumTuple
 
+-- Handles creating and caching intermediate results.
 makeResult urn totals cache =
   (totals, Map.insert (List.sort urn) totals cache)
 
@@ -39,14 +39,13 @@ computeCounts urn = totals
    where (totals, _) = computeCountsWithCache urn Map.empty
 
 computeCountsWithCache urn cache = 
-  if allBallsPicked urn then singleResult urn cache
-  else multiResult urn cache
-
-singleResult urn cache = makeResult urn ((countColorsPicked urn), 1) cache
-multiResult urn cache =
-  makeResult urn totals newCache
-  where children = getChildren urn
-        (totals, newCache) = mergeChildResults urn cache children
+  case Map.lookup (List.sort urn) cache of
+    Just totals -> (totals, cache)
+    Nothing -> if allBallsPicked urn 
+      then makeResult urn (leaf urn) cache
+      else makeResult urn totals newCache
+      where children = getChildren urn
+            (totals, newCache) = mergeChildResults urn cache children
 
 mergeChildResults urn cache (child:rest) = 
   if rest == [] then (childTotals, childCache)
@@ -54,13 +53,10 @@ mergeChildResults urn cache (child:rest) =
   where (childTotals, childCache) = computeCountsWithCache child cache
         (restTotals, restCache) = mergeChildResults urn childCache rest
 
-startingUrn = replicate numColors numPerColor 
-
 main = do 
-  --print $ computeCounts startingUrn
-  let ((totalColorsPicked, totalLeaves), cache) = computeCountsWithCache startingUrn Map.empty
+  let startingUrn = replicate numColors numPerColor 
+  let (totalColorsPicked, totalLeaves) = computeCounts startingUrn
   putStrLn $ "Total colors = " ++ show totalColorsPicked
   putStrLn $ "Total leaves = " ++ show totalLeaves
   putStrLn $ "Average = " ++ show ((fromIntegral totalColorsPicked) / (fromIntegral totalLeaves))
-  print cache
 
