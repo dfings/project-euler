a40fdd2c30bd5fb1acaa64289639d343842ce691
Dan Fingal-Surma <dfingal@gmail.com>
2015-04-17 00:53:39 -0700

    Go impl for problem 4

---
+++ go/problem_4.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func isPalindrome(s string) bool {
+	i, j := 0, len(s) - 1
+	for {
+		if s[i] != s[j] {
+			return false
+		} else if i > j {
+			return true
+		}
+		i = i + 1
+		j = j - 1
+	}
+	return true
+}
+
+func main() {
+	max := 0
+	for i := 999; i > 99; i-- {
+		for j := 999; j > 99; j-- {
+			current := i * j
+			if isPalindrome(strconv.Itoa(current)) && current > max {
+				max = current
+			}
+		}
+	}
+	fmt.Printf("%d\n", max)
+}
