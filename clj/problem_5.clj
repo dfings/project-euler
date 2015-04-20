e16f40333d8d2e471706087c7196f3b35fddebf3
Dan Fingal-Surma <dfingal@gmail.com>
2015-04-19 23:32:41 -0700

    Remove lcm-helper, simplify

--- clj/problem_5.clj
+++ clj/problem_5.clj
@@ -1,16 +1,14 @@
-(defn is-factor [fac n] (zero? (mod n fac)))
-
-(defn lcm-helper [x y i]
-  (let [x' (if (is-factor i x) (/ x i) x)
-        y' (if (is-factor i y) (/ y i) y)]
-    (list x' y' (if (or (not= x x') (not= y y')) [i] []))))
+(defn divide-if-factor [x i] 
+  (if (zero? (mod x i)) (/ x i) x))
 
 (defn lcm [x y]
   (loop [x' x y' y i 2 factors []]
     (if (= 1 (max x' y'))
       (reduce * factors)
-      (let [[x'' y'' factor] (lcm-helper x' y' i)
-            i' (if (empty? factor) (inc i) i)]
+      (let [x'' (divide-if-factor x' i)
+            y'' (divide-if-factor y' i)
+            i' (if (and (= x' x'') (= y' y'')) (inc i) i)
+            factor (if (= i i') [i] [])]
         (recur x'' y'' i' (concat factor factors))))))
 
 (defn main []
