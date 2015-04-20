a41e96ae965be31611bcc75bf96f7fa38a56ce09
Dan Fingal-Surma <dfingal@gmail.com>
2015-04-19 21:07:39 -0700

    Fix string reversal.

--- fs/problem_4.fs
+++ fs/problem_4.fs
@@ -2,7 +2,7 @@ let multiplyByAll3DigitNumbers x = List.map ((*) x) [100..999]
 let productsOfTwo3DigitNumbers = 
   [100..999] |> List.map multiplyByAll3DigitNumbers |> List.concat |> List.sort |> List.rev
 
-let reverseString (s:string) = new string(s.Reverse())
+let reverseString (s:string) = new string(Array.rev (s.ToCharArray()))
 let isPalindrome x = 
   let s = x.ToString()
   s = reverseString s
