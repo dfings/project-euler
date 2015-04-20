c40af0a8a804208ebb4c1d9fdde50e8982e8caa5
Dan Fingal-Surma <dfingal@gmail.com>
2015-04-19 22:45:17 -0700

    2nd tail recursive solution

--- clj/problem_3.clj
+++ clj/problem_3.clj
@@ -2,11 +2,9 @@
 ;; get very deep.
 (defn factor-with [n i]
   (loop [n n i i]
-    (if (= n 1)
-      []
-      (if (= (mod n i) 0)
-        (cons i (factor-with (/ n i) i))  ;; Can't recur here.
-        (recur n (+ i 1))))))
+    (cond (= n 1) []
+          (= (mod n i) 0) (cons i (factor-with (/ n i) i))  ;; Can't recur here.
+          :else (recur n (+ i 1)))))
 
 (defn factor [n]
   (reverse (factor-with n 2)))
@@ -18,16 +16,29 @@
     (list n (+ i 1) factors)))
 
 (defn factor-tail-recursive [n]
-  (loop [n n 
-         i 2 
-         factors []]
-    (if (= n 1)
+  (loop [n' n i 2 factors []]
+    (if (= n' 1)
       factors
-      (let [[n' i' factors'] (get-tail-args n i factors)]
-        (recur n' i' factors')))))
+      (let [[n'' i' factors'] (get-tail-args n' i factors)]
+        (recur n'' i' factors')))))
+
+;; Second solution using full tail recursion.
+(defn factor-by [n i]
+  (loop [n' n factor-count 0]
+    (if (> (mod n' i) 0)
+      (list n' (replicate factor-count i))
+      (recur (/ n' i) (+ factor-count 1)))))
+
+(defn factor-tail-recursive-2 [n]
+  (loop [n' n i 2 factors []]
+    (if (= n' 1)
+      factors
+      (let [[n'' new-factors] (factor-by n' i)]
+        (recur n'' (+ i 1) (concat new-factors factors))))))
 
 (defn main []
   (printf "%d\n" (first (factor 600851475143)))
-  (printf "%d\n" (first (factor-tail-recursive 600851475143))))
+  (printf "%d\n" (first (factor-tail-recursive 600851475143)))
+  (printf "%d\n" (first (factor-tail-recursive-2 600851475143))))
 
 (main)
