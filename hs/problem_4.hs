be097e890f46bbe8a220f2611410c047bac09cf3
Dan Fingal-Surma <dfingal@gmail.com>
2015-04-19 21:05:34 -0700

    Don't compute the string twice.

--- hs/problem_4.hs
+++ hs/problem_4.hs
@@ -5,6 +5,6 @@ multiplyByAll3DigitNumbers x = map ((*) x) [100..999]
 productsOfTwo3DigitNumbers =
   reverse $ List.sort $ concat $ map multiplyByAll3DigitNumbers [100..999]
 
-isPalindrome x = show x == reverse (show x)
+isPalindrome x = s == reverse s where s = show x
 
 main = print $ head $ filter isPalindrome productsOfTwo3DigitNumbers
