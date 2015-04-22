8d08073d73fb31236542a428b3d41f4cc84e7e91
Dan Fingal-Surma <dfingal@gmail.com>
2015-04-22 10:38:01 -0700

    Second solution to problem 7

---
+++ py/problem_7b.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python 
+
+def primes():
+  i = 2
+  sieve = {}
+
+  while True:
+    res = sieve.get(i)
+    if not res:
+      yield i
+      sieve[i * i] = [i]
+    else:
+      del sieve[i]
+      for j in res:
+        sieve.setdefault(i + j, []).append(j)      
+    i += 1
+
+
+def get_index(iterator, i):
+  for j, elt in enumerate(iterator):
+    if i == j:
+      return elt
+
+
+def main():
+  print get_index(primes(), 10000)
+  
+
+if __name__ == '__main__':
+  main()
