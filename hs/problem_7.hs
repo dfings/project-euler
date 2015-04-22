5291ef514c2bcae4845021cbd47b355707ba6e67
Dan Fingal-Surma <dfingal@gmail.com>
2015-04-22 02:38:10 -0700

    isNot => not is

--- hs/problem_7.hs
+++ hs/problem_7.hs
@@ -1,11 +1,11 @@
-isNotDivisibleBy _ [] = True
-isNotDivisibleBy i (p:ps) =
-  if mod i p == 0 then False
-  else if p * p > i then True
-  else isNotDivisibleBy i ps
+isDivisibleByAny _ [] = False
+isDivisibleByAny i (p:ps) =
+  if mod i p == 0 then True
+  else if p * p > i then False
+  else isDivisibleByAny i ps
 
 getNextPrime ps = getNextPrimeFrom (1 + last ps) ps
-  where getNextPrimeFrom i ps = if isNotDivisibleBy i ps 
+  where getNextPrimeFrom i ps = if not (isDivisibleByAny i ps)
         then i 
         else getNextPrimeFrom (i + 1) ps
 
