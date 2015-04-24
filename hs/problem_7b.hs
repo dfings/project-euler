b92ebaf9b2c2ed7e0cbea96302c37dfb6701f974
Dan Fingal-Surma <dfingal@gmail.com>
2015-04-23 17:52:21 -0700

    Implement sieve for Haskell, 10x faster.

---
+++ hs/problem_7b.hs
@@ -0,0 +1,19 @@
+import qualified Data.Map.Strict as Map
+
+addToSieve sieve _ [] = sieve
+addToSieve sieve nonPrime (p:ps) =
+  addToSieve sieve'' nonPrime ps
+  where composite = nonPrime + p
+        existingFactors = Map.findWithDefault [] composite sieve
+        sieve' = Map.insert composite (p : existingFactors) sieve
+        sieve'' = Map.delete nonPrime sieve'
+
+nextPrime (sieve, i) =
+  case Map.lookup i sieve of
+    Nothing -> (Map.insert (i * i) [i] sieve, i)
+    Just existingFactors -> nextPrime ((addToSieve sieve i existingFactors), (i + 1))
+
+primes = gen (nextPrime (Map.empty, 2))
+  where gen (sieve, currentPrime) = currentPrime : gen (nextPrime (sieve, currentPrime + 1))
+
+main = print $ primes !! 10000
