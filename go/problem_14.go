ce2a6ceb0f36069ce0a9ee2fe3e02f81018e7f2e
Dan Fingal-Surma <dfingal@gmail.com>
2015-04-26 00:30:40 -0700

    Go solution to problem 14

---
+++ go/problem_14.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+var cache = make(map[int]int)
+
+func getNext(n int) int {
+  if n % 2 == 0 {
+    return n / 2
+  } else {
+    return 3 * n + 1
+  }
+}
+
+func computeAndCache(n int) int {
+  if n == 1 {
+    return 1
+  }
+  length, ok := cache[n]
+  if ok {
+    return length
+  } else {
+    next := getNext(n)
+    length = 1 + computeAndCache(next)
+    cache[n] = length
+    return length
+  }
+}
+
+func main() {
+  max_n := -1
+  max_length := 0
+  for n := 1; n <= 1000000; n++ {
+    length := computeAndCache(n)
+    if length > max_length {
+      max_n = n
+      max_length = length
+    }
+  }
+  fmt.Printf("%d %d\n", max_length, max_n)
+}
