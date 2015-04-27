0cd75c9fab89bbe0859afccc9b0173209bbd254b
Dan Fingal-Surma <dfingal@gmail.com>
2015-04-27 00:26:23 -0700

    Change find-triple to just triple

--- clj/problem_9.clj
+++ clj/problem_9.clj
@@ -1,12 +1,11 @@
-(defn find-triple []
+(def triple (first
   (for [a (range 1 333)
         b (range a 500)
         :let [c (- 1000 a b)]
         :when (= (+ (* a a) (* b b)) (* c c))]
-   (list a b c)))
+   (list a b c))))
 
 (defn main []
-  (let [triple (first (find-triple))]
-    (printf "%s -> %s\n" triple (reduce * triple))))
+  (printf "%s -> %s\n" triple (reduce * triple)))
 
 (main)
