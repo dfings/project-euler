15a3b4d5c01e0eddc65e4fc0d2132838862eff49
Dan Fingal-Surma <dfingal@gmail.com>
2015-04-27 01:25:56 -0700

    Update range

--- go/problem_9.go
+++ go/problem_9.go
@@ -3,12 +3,9 @@ package main
 import "fmt"
 
 func main() {
-  for a := 1; a < 1000; a++ {
-    for b := a + 1; b < 1000; b++ {
+  for a := 1; a < 333; a++ {
+    for b := a + 1; b < 500 - a; b++ {
       c := 1000 - a - b
-      if c < b {
-        continue
-      }
       if a * a + b * b == c * c {
         fmt.Printf("%d %d %d -> %d\n", a, b, c, a * b * c)
         return
