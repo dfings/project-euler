f2de2096c3bd6d003ec1bd4f5c31d602f679323c
Dan Fingal-Surma <dfingal@gmail.com>
2015-05-02 00:30:10 -0700

    Go problem 13

---
+++ go/problem_10.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+  "bufio"
+  "fmt"
+  "math/big"
+  "os"
+)
+
+func main() {
+	file, _ := os.Open("../data/problem_10_data")
+	defer file.Close()
+
+	scanner := 	bufio.NewScanner(file)
+	sum := big.NewInt(1)
+	for scanner.Scan() {
+		var val big.Int
+		val.UnmarshalText(scanner.Bytes())
+		sum.Add(sum, &val)
+	}
+	fmt.Println(sum.String()[0:10])
+}
