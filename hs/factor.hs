f394ae9375237f6e04052715f6e3baf8450a25a0
Dan Fingal-Surma <dfingal@gmail.com>
2015-05-02 22:56:59 -0700

    Modularize factors and primes

---
+++ hs/factor.hs
@@ -0,0 +1,7 @@
+module Factor (factor) where
+
+factor num = factor' num 2
+  where factor' 1 _ = []
+        factor' x i = if (mod x i) == 0 
+          then i : factor' (div x i) i 
+          else factor' x (i + 1)
--- hs/problem_3.hs
+++ hs/problem_3.hs
@@ -1,7 +1,3 @@
-factor num = factor' num 2
-  where factor' 1 _ = []
-        factor' x i = if (mod x i) == 0 
-          then i : factor' (div x i) i 
-          else factor' x (i + 1)
+import Factor  -- factor.hs
 
 main = print $ head $ reverse $ factor $ 600851475143
