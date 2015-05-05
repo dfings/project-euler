c79f67ac988db8b05d662012e86c6e10663aade2
Dan Fingal-Surma <dfingal@gmail.com>
2015-05-05 00:59:28 -0700

    Remove stray print

--- go/problem_12.go
+++ go/problem_12.go
@@ -6,8 +6,6 @@ func main() {
   sum := 0
   for i := 1; true; i++ {
     sum += i
-    factors := Factors(sum)
-    fmt.Printf("%v %v\n", sum, factors)
     if len(Factors(sum)) > 500 {
     	fmt.Printf("%d\n", sum)
     	break
