43150fe919ba0025207ec7b287542e7543c74d9d
Dan Fingal-Surma <dfingal@gmail.com>
2015-05-05 00:17:06 -0700

    Prime generator using atom and swap!

--- clj/primes.clj
+++ clj/primes.clj
@@ -1,5 +1,7 @@
 (ns primes)
 
+;; Version 1 doesn't use atoms or swap!
+
 (defn add-to-sieve [sieve non-prime factors]
   (if (empty? factors)
     sieve
@@ -22,4 +24,33 @@
   ([current-prime sieve]
       (lazy-seq (cons
         current-prime 
-        (apply primes (next-prime (+ current-prime 1) sieve))))))
+        (apply primes (next-prime (inc current-prime) sieve))))))
+
+;; Version 2 uses atoms and swap!
+
+(defn add-to-sieve2 [sieve non-prime factors]
+  (doseq [factor factors]
+    (let [composite (+ non-prime factor)
+          existing-factors (get @sieve composite [])]
+      (do
+        (swap! sieve assoc composite (cons factor existing-factors))
+        (swap! sieve dissoc sieve non-prime)))))
+
+(defn next-prime2 [i sieve]
+  (let [factors (get @sieve i)]
+    (if (= factors nil)
+      (do 
+        (swap! sieve assoc (* i i) [i])
+        i)
+      (do
+        (add-to-sieve2 sieve i factors)        
+        (recur (inc i) sieve)))))
+
+(defn primes2
+  ([] 
+    (let [sieve (atom {})]
+      (primes2 (next-prime2 2 sieve) sieve)))
+  ([current-prime sieve]
+      (lazy-seq (cons
+        current-prime 
+        (primes2 (next-prime2 (inc current-prime) sieve) sieve)))))
