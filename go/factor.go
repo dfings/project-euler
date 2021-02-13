f4f1eb487862828b7fd56d157deb8fa27b16e474
Dan Fingal-Surma <dfingal@gmail.com>
2021-02-13 14:05:23 -0800

    Update factor.go

--- go/factor.go
+++ go/factor.go
@@ -2,17 +2,13 @@ package main
 
 func PrimeFactor(n int) []int {
 	var factors []int
-	primes := NewPrimeGenerator()
-	for {
-		prime := primes.Next()
-		if n%prime == 0 {
-			factors = append(factors, n)
-				for n%prime == 0 {
-				n = n / prime
-			}
-		}
-		if n == 1 {
-			break
+	i := 2
+	for n > 1 {
+		if n%i == 0 {
+			n = n / i
+			factors = append(factors, i)
+		} else {
+			i = i + 1
 		}
 	}
 	return factors
