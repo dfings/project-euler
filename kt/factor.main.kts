24448da5fe2c3224cfc25ecc941a27d4a0a6d93a
Dan Fingal-Surma <dfingal@gmail.com>
2021-02-13 15:13:55 -0800

    kt functional factoring

--- kt/factor.main.kts
+++ kt/factor.main.kts
@@ -10,3 +10,18 @@ fun primeFactors(value: Long): Sequence<Long> = sequence {
     }
   }
 }
+
+data class FactorState(val n: Long, val i: Long, val out: List<Long>) {
+  fun next() = if (n % i == 0L) hit() else miss()
+  fun miss() = FactorState(n, i + 1, out)
+  fun hit() = FactorState(n / i, i, out + i)
+}
+
+tailrec fun primeFactorsStateMachine(state: FactorState): FactorState = 
+  when (state.n) {
+    1L -> state
+    else -> primeFactorsStateMachine(state.next())
+  }
+
+fun primeFactorsFunctional(value: Long): List<Long> =
+  primeFactorsStateMachine(FactorState(value, 2, listOf<Long>())).out
