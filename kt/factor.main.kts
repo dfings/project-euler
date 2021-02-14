4aff90c5f165def31386f435cdfecb2df541fecd
Dan Fingal-Surma <dfingal@gmail.com>
2021-02-13 16:46:32 -0800

    Proper lazy functional factoring impl

--- kt/factor.main.kts
+++ kt/factor.main.kts
@@ -11,6 +11,7 @@ fun primeFactors(value: Long): Sequence<Long> = sequence {
   }
 }
 
+
 data class FactorState(val n: Long, val i: Long, val out: List<Long>) {
   fun next() = if (n % i == 0L) hit() else miss()
   fun miss() = FactorState(n, i + 1, out)
@@ -23,5 +24,16 @@ tailrec fun primeFactorsStateMachine(state: FactorState): FactorState =
     else -> primeFactorsStateMachine(state.next())
   }
 
-fun primeFactorsFunctional(value: Long): List<Long> =
+fun primeFactorsTailRec(value: Long): List<Long> =
   primeFactorsStateMachine(FactorState(value, 2, listOf<Long>())).out
+
+
+// Note converting Sequence to List results in StackOverflowError
+fun primeFactorsLazyWith(n: Long, i: Long): Sequence<Long> = when {
+  n == 1L -> sequenceOf<Long>()
+  n % i == 0L -> sequenceOf(i) + primeFactorsLazyWith(n / i, i)
+  else -> primeFactorsLazyWith(n, i + 1)
+}
+
+fun primeFactorsLazy(value: Long): Sequence<Long> =
+  primeFactorsLazyWith(value, 2)
--- kt/problem_3.main.kts
+++ kt/problem_3.main.kts
@@ -5,4 +5,5 @@
 @file:Import("factor.main.kts")
 
 println(primeFactors(600851475143).last())
-println(primeFactorsFunctional(600851475143).last())
+println(primeFactorsTailRec(600851475143).last())
+println(primeFactorsLazy(600851475143).last())
