2276168c93a38348abe396e31fbfb063d22de6b9
Dan Fingal-Surma <dfingal@gmail.com>
2021-02-13 21:56:15 -0800

    Nested fns

--- clj/factor.clj
+++ clj/factor.clj
@@ -1,21 +1,19 @@
 (ns factor)
 
-(defn prime-factor-with [n i]
-  (cond 
-    (= n 1) []
-    (zero? (mod n i)) (lazy-seq (cons i (prime-factor-with (/ n i) i)))
-    :else (recur n (inc i))))
-
 (defn prime-factor [n]
+  (defn prime-factor-with [n i]
+    (cond 
+      (= n 1) []
+      (zero? (mod n i)) (lazy-seq (cons i (prime-factor-with (/ n i) i)))
+      :else (recur n (inc i))))
   (reverse (prime-factor-with n 2)))
 
-(defn factor-with [n i]
-  (let [i-squared (* i i)]
-    (cond
-      (> i-squared n) []
-      (= i-squared n) [i]
-      (zero? (mod n i)) (lazy-seq (cons i (cons (/ n i) (factor-with n (inc i)))))
-      :else (recur n (inc i)))))
-
 (defn factors [n]
+  (defn factor-with [n i]
+    (let [i-squared (* i i)]
+      (cond
+        (> i-squared n) []
+        (= i-squared n) [i]
+        (zero? (mod n i)) (lazy-seq (cons i (cons (/ n i) (factor-with n (inc i)))))
+        :else (recur n (inc i)))))
   (factor-with n 2))
