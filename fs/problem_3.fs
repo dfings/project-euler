8b1f1ed32704c91872b7f9859e85baf6569bd2c2
Dan Fingal-Surma <dfingal@gmail.com>
2021-02-13 21:24:07 -0800

    Nested function in F#

--- fs/problem_3.fs
+++ fs/problem_3.fs
@@ -1,8 +1,8 @@
-let rec factorWith num i =
-  if num = 1L then []
-  elif num % i = 0L then i :: factorWith (num / i) i
-  else factorWith num (i + 1L)
-
-let factor num = factorWith num 2L
+let factor num = 
+  let rec factor' num i =
+    if num = 1L then []
+    elif num % i = 0L then i :: factor' (num / i) i
+    else factor' num (i + 1L)
+  factor' num 2L
 
 printfn "%d" (factor 600851475143L |> List.rev |> List.head)
