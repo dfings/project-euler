cc497f49da9e985269753241cf6c50d844670e83
Dan Fingal-Surma <dfingal@gmail.com>
2021-02-26 11:56:07 -0800

    Don't mutate the urn

--- kt/problem_493.main.kts
+++ kt/problem_493.main.kts
@@ -8,11 +8,11 @@ val NUM_PER_COLOR = 10
 val NUM_TO_PICK = 20
 val SUM_ALL_PICKED = NUM_COLORS * NUM_PER_COLOR - NUM_TO_PICK
 
-class Urn {
-  val slots =  Array<Int>(NUM_COLORS) { NUM_PER_COLOR }
+class Urn(val slots: List<Int>) {
   fun colorsPicked(): Int = slots.count { it < NUM_PER_COLOR }
   fun allPicked() = slots.sum() == SUM_ALL_PICKED
   fun cacheKey() = slots.sorted()
+  fun pick(color: Int) = Urn(slots.mapIndexed { i, count -> if (i == color) count - 1 else count })
 }
 
 data class ChildStats(val totalColors: BigDecimal, val totalLeaves: BigDecimal) {
@@ -28,18 +28,17 @@ fun urnStats(urn: Urn): ChildStats = urnCache.getOrPut(urn.cacheKey()) {
     var totalLeaves = 0.toBigDecimal()
     for (color in 0..NUM_COLORS-1) {
       for (unused in 1..urn.slots[color]) {
-        urn.slots[color] -= 1
-        val childStats = urnStats(urn)
+        val child = urn.pick(color)
+        val childStats = urnStats(child)
         totalColors += childStats.totalColors
         totalLeaves += childStats.totalLeaves
-        urn.slots[color] += 1
       }
     }
     ChildStats(totalColors, totalLeaves)
   }
 }
 
-val urn = Urn()
+val urn = Urn(IntArray(NUM_COLORS) { NUM_PER_COLOR }.asList())
 val stats = urnStats(urn)
 println("Total colors = ${stats.totalColors}")
 println("Total picks = ${stats.totalLeaves}")
