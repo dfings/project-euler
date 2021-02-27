18f414d43a6adc4c0880bb13d14e41623002d252
Dan Fingal-Surma <dfingal@gmail.com>
2021-02-26 18:51:41 -0800

    Create problem_9.py

---
+++ py/problem_9.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python 
+
+from math import prod
+
+def find_right_triangle():
+  for a in range(1, 333):
+    for b in range(a, 500):
+      c = 1000 - a - b
+      if a * a + b * b == c * c:
+        return (a, b, c)
+
+
+triangle = find_right_triangle()
+print(triangle)
+print(prod(triangle))
