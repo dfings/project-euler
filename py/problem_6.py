bbb6e8d86a828775c2f81ac417bcb59ea9384588
Dan Fingal-Surma <dfingal@gmail.com>
2021-02-26 18:24:12 -0800

    Create problem_6.py

---
+++ py/problem_6.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+def square(x: int) -> int:
+  return x * x
+
+
+def square_of_sum(x: int) -> int:
+  return square(sum(range(1, x + 1)))
+
+
+def sum_of_squares(x: int) -> int:
+  return sum(map(square, range(1, x + 1)))
+
+
+print(square_of_sum(100) - sum_of_squares(100))
