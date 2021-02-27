0661c6e5e456676b23768a3929aa444707a5fba4
Dan Fingal-Surma <dfingal@gmail.com>
2021-02-26 18:14:41 -0800

    Create problem_4.py

---
+++ py/problem_4.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from itertools import product
+
+def products_of_three_digit_numbers():
+  return map(sum, product(range(100, 1000), range(100, 1000)))
+
+
+def is_palindrome(value):
+  value_str = str(value)
+  return value_str == value_str[::-1]
+
+
+print(max(filter(is_palindrome, products_of_three_digit_numbers())))
