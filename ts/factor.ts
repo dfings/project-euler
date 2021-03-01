fff5a492277e13c7cc7222e229327a8f27dfe2d1
Dan Fingal-Surma <dfingal@gmail.com>
2021-03-01 10:45:49 -0800

    Update factor.ts

--- ts/factor.ts
+++ ts/factor.ts
@@ -4,9 +4,9 @@ export function prime_factors(n: number): Array<number> {
   while (n > 1) {
     if (n % i == 0) {
       factors.push(i);
-      n /= i
+      n /= i;
     } else {
-      i += 1
+      i += 1;
     }
   }
   return factors;
