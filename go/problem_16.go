3b7319fceffb5af770e5c9bc4c126b283bbe9a62
Dan Fingal-Surma <dfingal@gmail.com>
2021-03-02 15:42:37 -0800

    Update problem_16.go

--- go/problem_16.go
+++ go/problem_16.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+	"math/big"
+)
+
+func main() {
+	zero, ten, sum := big.NewInt(0), big.NewInt(10), big.NewInt(0)
+	var n, r big.Int
+	n.Exp(big.NewInt(2), big.NewInt(1000), nil)
+	for n.Cmp(zero) > 0 {
+		n.DivMod(&n, ten, &r)
+		sum.Add(sum, &r)
+	}
+	fmt.Printf("%s\n", sum.String())
+}
