7c04d175696b2dca564fe9ab80c27c0572a92ef3
Dan Fingal-Surma <dfingal@gmail.com>
2021-03-08 13:43:26 -0800

    Update problem_493.fs

--- fs/problem_493.fs
+++ fs/problem_493.fs
@@ -10,35 +10,34 @@ let numPerColor = 10
 let numToPick = 20
 
 (* Determines if we've picked all the balls we need to from an urn. *)
-let allBallsPicked = List.sum >> (=) (numColors * numPerColor - numToPick)
+let allBallsPicked = Seq.sum >> (=) (numColors * numPerColor - numToPick)
 
 (* Counts the number of distinct colors that have been picked from the urn. *)
-let countColorsPicked = List.filter ((<>) numPerColor) >> List.length
+let countColorsPicked = Seq.filter ((<>) numPerColor) >> Seq.length
 
-(* Generates one new urn state for each possible ball that could be picked. *)
-let rec childGenerator head ballCount tail =
-  let remaining() = childGenerator (head @ [ballCount]) (List.head tail) (List.tail tail)
-  let oneChildStatePerBall = List.replicate ballCount
-  let allChildStatesForColor = 
-      if ballCount = 0 then [] 
-      elif tail = [] then oneChildStatePerBall (head @ [ballCount - 1])
-      else oneChildStatePerBall (head @ [ballCount - 1] @ tail)
-  if tail = [] then allChildStatesForColor else allChildStatesForColor @ remaining()
-
-(* Helper for using childGenerator on an urn. *)
-let getChildren urn = childGenerator [] (List.head urn) (List.tail urn)
+(* Returns a new urn with a ball of the given color removed. *)
+let pick color urn =
+  urn 
+  |> List.indexed 
+  |> List.map(fun (i, v) -> if (i = color) then v - 1 else v)
 
 (* Tuple support. *)
 let sumTuple (a1, a2) (b1, b2) = a1 + b1, a2 + b2
 let bigIntTuple (a1:int, a2:int) = (bigint a1, bigint a2)
-let sumBigIntTuples = List.fold sumTuple (bigIntTuple (0, 0))
+let sumBigIntTuples = Seq.reduce sumTuple
 
 (* Computes (totalColorsPicked, totalLeaves) for leaves rooted at this subtree. *)
 let rec computeCounts = Memoize.memoizeWithCacheKey List.sort (fun urn ->
   (* If this is a leaf, then we can just count the colors directly. *)
   if allBallsPicked urn then bigIntTuple ((countColorsPicked urn), 1)
   (* Otherwise we need to sum up the values of all leaves rooted at this subtree. *)  
-  else getChildren urn |> List.map computeCounts |> sumBigIntTuples  
+  else 
+    [0..numColors-1] 
+    |> Seq.map(fun color -> 
+      [1..(List.item color urn)] 
+      |> Seq.map(fun _ -> computeCounts (pick color urn)))
+    |> Seq.concat
+    |> sumBigIntTuples
 )
 
 let startingUrn = List.replicate numColors numPerColor 
