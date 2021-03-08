668c0842bffdeff540b9e3b33093f500133b75b6
Dan Fingal-Surma <dfingal@gmail.com>
2021-03-08 01:21:33 -0800

    Update problem_493.ex

--- ex/problem_493.ex
+++ ex/problem_493.ex
@@ -13,28 +13,9 @@ defmodule Problem493 do
   def has_been_picked(count), do: count < @num_per_color
   def count_colors_picked(urn), do: urn |> Enum.filter(&has_been_picked/1) |> Enum.count
 
-  # Gets the next urn states giving that we're considering picking a ball at 
-  # the given position that has the given number currentlyl left in the urn.
-  def get_next_states(head, ball_count, tail) do
-    cond do
-      ball_count == 0 -> []
-      tail == [] -> List.duplicate(head ++ [ball_count - 1], ball_count)
-      true -> List.duplicate(head ++ [ball_count - 1] ++ tail, ball_count)
-    end
+  def pick(urn, color) do
+    urn |> Enum.with_index() |> Enum.map(fn {v, i} -> if (i == color) do v - 1 else v end end)
   end
-
-  # Generates one new urn state for each possible ball that could be picked.
-  def child_gen(head, ball_count, tail) do
-    next_states = get_next_states(head, ball_count, tail)
-    case tail do
-      [] -> next_states 
-      [nextball_count | next_tail] -> 
-        next_states ++ child_gen(head ++ [ball_count], nextball_count, next_tail)
-    end
-  end
- 
-  # Helper for using childGenerator on an urn.
-  def generate_children([head | tail]), do: child_gen([], head, tail)
 
   # Tuple support.
   def sum_tuples({a1, a2}, {b1, b2}), do: {a1 + b1, a2 + b2}
@@ -51,12 +32,16 @@ defmodule Problem493 do
         {count_colors_picked(urn), 1} 
       else
         # Otherwise we need to sum up the values of all leaves rooted at this subtree.
-        child_counts = Enum.map(generate_children(urn), &compute_counts/1)
-        List.foldl(child_counts, {0, 0}, &sum_tuples/2)
+        (0..@num_colors-1) 
+        |> Enum.flat_map(fn color -> 
+          (1..color) 
+          |> Enum.map(fn _ -> compute_counts(pick(urn, color)) end) 
+          end)
+        |> Enum.reduce(&sum_tuples/2)
       end
       Process.put(key, res)
       res
-    end
+    end  
   end
 
   def main do
