4d418f2b86b62120342865bfc1c8c71c8a2cc461
Dan Fingal-Surma <dfingal@gmail.com>
2021-04-11 23:32:02 -0700

    Call unwrap

--- rs/problem_3/src/main.rs
+++ rs/problem_3/src/main.rs
@@ -3,8 +3,5 @@ extern crate common;
 use common::prime_factors;
 
 fn main() {
-    let factors = prime_factors(600851475143);
-    if let Some(x) = factors.last() {
-        println!("{}", x);
-    }
+    println!("{}", prime_factors(600851475143).last().unwrap());
 }
