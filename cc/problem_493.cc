78e7cf20006f4eb66f870b5e3739b791966883b7
Dan Fingal-Surma <dfingal@gmail.com>
2025-11-19 01:17:49 -0800

    Fix formatting

--- cc/problem_493.cc
+++ cc/problem_493.cc
@@ -11,56 +11,56 @@ using Counts = std::pair<double, double>;
 using Urn = std::vector<int>;
 
 class Solver {
- public:
-   Solver() : urn_(kNumColors, kNumPerColor) {}
-   
-   int CountColors() {
-     int count = 0;
-     for (auto val : urn_) {
-       if (val != kNumPerColor) {
-          ++count;
+public:
+  Solver() : urn_(kNumColors, kNumPerColor) {}
+
+  int CountColors() {
+    int count = 0;
+    for (auto val : urn_) {
+      if (val != kNumPerColor) {
+        ++count;
+      }
+    }
+    return count;
+  }
+
+  Counts ComputeCounts() {
+    if (balls_picked_ == kNumToPick) {
+      return Counts{1, CountColors()};
+    }
+
+    Urn encoded = urn_;
+    std::sort(encoded.begin(), encoded.end());
+    auto it = memoized_.find(encoded);
+    if (it != memoized_.end()) {
+      return it->second;
+    }
+
+    Counts counts{0, 0};
+    ++balls_picked_;
+    for (int i = 0; i < kNumColors; ++i) {
+      for (int j = 0; j < kNumPerColor; ++j) {
+        if (urn_[i] > j) {
+          --urn_[i];
+          Counts returned = ComputeCounts();
+          counts.first += returned.first;
+          counts.second += returned.second;
+          ++urn_[i];
         }
-     }
-     return count;
-   }
-  
-   Counts ComputeCounts() {
-     if (balls_picked_ == kNumToPick) {
-       return Counts{1, CountColors()};
-     }
+      }
+    }
+    --balls_picked_;
+    memoized_.emplace(encoded, counts);
+    return counts;
+  }
 
-     Urn encoded = urn_;
-     std::sort(encoded.begin(), encoded.end());
-     auto it = memoized_.find(encoded);
-     if (it != memoized_.end()) {
-       return it->second;
-     } 
-
-     Counts counts{0, 0};
-     ++balls_picked_;
-     for (int i = 0; i < kNumColors; ++i) {
-       for (int j = 0; j < kNumPerColor; ++j) {
-         if (urn_[i] > j) {
-           --urn_[i];
-           Counts returned = ComputeCounts();
-           counts.first += returned.first;
-           counts.second += returned.second;
-           ++urn_[i];
-         }
-       }
-     }
-     --balls_picked_;
-     memoized_.emplace(encoded, counts);
-     return counts;
-   }
-    
- private:
-   int balls_picked_ = 0;
-   Urn urn_;
-   std::map<Urn, Counts> memoized_;
+private:
+  int balls_picked_ = 0;
+  Urn urn_;
+  std::map<Urn, Counts> memoized_;
 };
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   Solver solver;
   Counts counts = solver.ComputeCounts();
   printf("Total colors = %0.0f\n", counts.second);
